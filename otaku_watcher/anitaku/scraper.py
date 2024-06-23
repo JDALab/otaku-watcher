88626f66eb49b97491cf1280abcb5ed326d0f58c
lowpolyv2 <168635415+lowpolyv2@users.noreply.github.com>
2024-06-23 02:44:54 +0100

    feat, fix: add image preview support, fix some titles invisible and fix --limit support.

--- otaku_watcher/anitaku/scraper.py
+++ otaku_watcher/anitaku/scraper.py
@@ -2,11 +2,13 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import List, Optional, Dict, Generator, Any
+    from typing import List, Optional, Dict, Iterable
 
     from mov_cli import Config
     from mov_cli.http_client import HTTPClient
     from mov_cli.scraper import ScraperOptionsT
+
+    from bs4 import Tag
 
 import re
 
@@ -22,10 +24,73 @@ class AnitakuScraper(Scraper):
         self.base_url = "https://anitaku.so"
         super().__init__(config, http_client, options)
 
-    def search(self, query: str, limit: int = 20) -> List[Metadata]:
-        query = query.replace(' ', '-')
-        results = self.__results(query, limit)
-        return results
+    def search(self, query: str, limit: Optional[int] = None) -> Iterable[Metadata]:
+        pagination = 1
+        result_count = 0
+
+        limit = 20 if limit is None else limit
+
+        while True:
+            req = self.http_client.get(f"{self.base_url}/search.html?keyword={query}&page={pagination}")
+            soup = self.soup(req)
+            items: List[Tag] = soup.find("ul", {"class": "items"}).findAll("li")
+
+            if len(items) == 0:
+                break
+
+            for item in items:
+                title_a_element = item.find("p", {"class": "name"}).find("a")
+
+                id = title_a_element["href"].split("/")[-1]
+                title = title_a_element.text.replace('"', '')
+                image_url = item.find("div", {"class": "img"}).find("img")["src"]
+
+                year_text = re.findall(r"(\d{4})", item.find("p", {"class": "released"}).text)
+
+                if year_text: # Animes without a year are not released
+                    year = year_text[0]
+                else:
+                    continue
+
+                page = self.http_client.get(self.base_url + f"/category/{id}")
+                _soup = self.soup(page)
+
+                episode_page = _soup.find("ul", {"id": "episode_page"})
+                li = episode_page.findAll("li")
+                last = li[-1].find("a")["ep_end"]
+
+                if last == "1":
+                    type = MetadataType.SINGLE
+                else:
+                    type = MetadataType.MULTI
+
+                info_body = _soup.find("div", {"class": "anime_info_body_bg"})
+
+                _p = info_body.findAll("p")
+
+                genres = _p[3].findAll("a")
+
+                yield Metadata(
+                    id = id,
+                    title = title,
+                    type = type,
+                    year = year,
+                    image_url = image_url,
+
+                    extra_func = lambda: ExtraMetadata(
+                        description = [str.strip(x) for x in _p[2].strings if str.strip(x) != ''][1].replace(r"\r\n", "\r\n"),
+                        alternate_titles = [],
+                        cast = None,
+                        genres = [i.text.split(" ")[-1] for i in genres]
+                    )
+                )
+
+                result_count += 1
+
+                if result_count >= limit:
+                    return None
+
+            pagination += 1
 
     def scrape(self, metadata: Metadata, episode: utils.EpisodeSelector) -> Multi | Single:
         req = self.http_client.get(self.base_url + f"/{metadata.id}-episode-{episode.episode}", redirect = True)
@@ -65,70 +130,7 @@ class AnitakuScraper(Scraper):
         last = int(li[-1].find("a")["ep_end"])
         return {1: last} # TODO: Return multiple seasons.
 
-    def __results(self, query: str, limit: int) -> Generator[Metadata, Any, None]:
-        pagination = 1
-        result_count = 0
-
-        while True:
-            req = self.http_client.get(f"{self.base_url}/search.html?keyword={query}&page={pagination}")
-            soup = self.soup(req)
-            items = soup.find("ul", {"class": "items"}).findAll("li")
-
-            if len(items) == 0:
-                break
-
-            for item in items:
-                id = item.find("a")["href"].split("/")[-1]
-                title = item.find("a")["title"]
-                year_text = re.findall(r"(\d{4})", item.find("p", {"class": "released"}).text)
-
-                if year_text: # Animes without a year are not released
-                    year = year_text[0]
-                else:
-                    continue
-
-                page = self.http_client.get(self.base_url + f"/category/{id}")
-                _soup = self.soup(page)
-
-                episode_page = _soup.find("ul", {"id": "episode_page"})
-                li = episode_page.findAll("li")
-                last = li[-1].find("a")["ep_end"]
-
-                if last == "1":
-                    type = MetadataType.SINGLE
-                else:
-                    type = MetadataType.MULTI
-
-                info_body = _soup.find("div", {"class": "anime_info_body_bg"})
-
-                _p = info_body.findAll("p")
-
-                genres = _p[3].findAll("a")
-
-                yield Metadata(
-                    id = id,
-                    title = title,
-                    type = type,
-                    year = year,
-
-                    extra_func = lambda: ExtraMetadata(
-                        description = [str.strip(x) for x in _p[2].strings if str.strip(x) != ''][1].replace(r"\r\n", "\r\n"),
-                        image_url = item.find("img")["src"],
-                        alternate_titles = [],
-                        cast = None,
-                        genres = [i.text.split(" ")[-1] for i in genres]
-                    )
-                )
-
-                if limit:
-                    result_count += 1
-
-                    if result_count >= limit:
-                        return None
-
-            pagination += 1
-
-    def __dood(self, url):
+    def __dood(self, url: str) -> str:
         video_id = url.split("/")[-1]
         webpage_html = self.http_client.get(
             f"https://dood.to/e/{video_id}", redirect = True
@@ -148,7 +150,7 @@ class AnitakuScraper(Scraper):
 
         return true_url
 
-    def __streamwish(self, url):
+    def __streamwish(self, url: str) -> str:
         req = self.http_client.get(url).text
         file = re.findall(r'file:"(.*?)"', req)[0]
 
